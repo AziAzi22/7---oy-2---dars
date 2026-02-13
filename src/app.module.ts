91f7c519cd7b4c60e5d50ba4aa300ec1e9c7f33c
AziAzi22 <aziazi22t@gmail.com>
2026-02-13 23:37:12 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Auth } from "./auth/entities/auth.entity";
+import { TodoModule } from "./todo/todo.module";
+import { Todo } from "./todo/entities/todo.entity";
+
+@Module({
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      username: "postgres",
+      port: 5432,
+      host: "localhost",
+      password: String(process.env.DB_PASSWORD),
+      database: String(process.env.DB_NAME),
+      autoLoadModels: true,
+      models: [Auth, Todo],
+      synchronize: true,
+      logging: false,
+    }),
+    TodoModule,
+  ],
+})
+export class AppModule {}
---
+++ src/auth/dto/create-auth.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNumber, IsString, Length } from "class-validator";
+import { Transform } from "class-transformer";
+
+export class CreateAuthDto {
+  @IsNumber()
+  id: number;
+
+  @IsString({ message: "username string bo'lishi kerak" })
+  @Transform(({ value }) => value.trim())
+  @Length(3, 50)
+  username: string;
+
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  password: string;
+}
